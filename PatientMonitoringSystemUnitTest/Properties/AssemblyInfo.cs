e81d790edad4ce2569fe9796d8c982e3db826e6a
Raymond Loke <junioraymond92@gmail.com>
2019-11-12 18:16:22 +0800

    add PatientMonitoringSystemUnitTest project and UnitTestAlarmHandler

---
+++ PatientMonitoringSystemUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PatientMonitoringSystemUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PatientMonitoringSystemUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dc7377a6-2772-4368-b2b4-7a2941352f15")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PatientMonitoringSystemUnitTest/UnitTestAlarmHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PatientMonitoringSystemUnitTest
+{
+    [TestClass]
+    public class UnitTestAlarmHandler
+    {
+        [TestMethod]
+        public void TestAddAlarm()
+        {
+            // alarm object
+            Alarm alarm = new Alarm();
+            Assert.IsInstanceOfType(alarm, typeof(object));
+        }
+    }
+}
