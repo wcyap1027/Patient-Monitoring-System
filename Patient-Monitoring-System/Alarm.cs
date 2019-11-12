1b364b79ec0020009c9c3c73e0738ff42505b285
Raymond Loke <junioraymond92@gmail.com>
2019-11-12 18:39:11 +0800

    update attribute of Alarm class

--- Patient-Monitoring-System/Alarm.cs
+++ Patient-Monitoring-System/Alarm.cs
@@ -8,5 +8,13 @@ namespace Patient_Monitoring_System
 {
     public class Alarm
     {
+        private string id;
+        private enum alarmLevel { lower, upper};
+        private DateTime dateTimeTrigger;
+        private DateTime dateTimeMuted;
+
+        public string Id { get => id; set => id = value; }
+        public DateTime DateTimeTrigger { get => dateTimeTrigger; set => dateTimeTrigger = value; }
+        public DateTime DateTimeMuted { get => dateTimeMuted; set => dateTimeMuted = value; }
     }
 }
