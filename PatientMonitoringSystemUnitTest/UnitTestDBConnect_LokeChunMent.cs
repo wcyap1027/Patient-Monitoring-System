138e00cb1bb2b2c5d428989382e72521f35e4e01
Raymond Loke <junioraymond92@gmail.com>
2019-11-12 20:52:38 +0800

    create UnitTestDBConnect_LokeChunMent to test the database connection

---
+++ PatientMonitoringSystemUnitTest/UnitTestDBConnect_LokeChunMent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Patient_Monitoring_System;
+
+namespace PatientMonitoringSystemUnitTest
+{
+    [TestClass]
+    public class UnitTestDBConnect_LokeChunMent
+    {
+        [TestMethod]
+        public void TestDatabase_LokeChunMent()
+        {
+            DBConnect_LokeChunMent dBc = new DBConnect_LokeChunMent();
+            string resp = dBc.connect();
+            Assert.AreEqual("Done", resp);
+        }
+    }
+}
