7df711601b889399d29c9447967f92f27a2b9275
wcyap1027 <wcyap9810@gmail.com>
2019-11-13 18:22:45 +0800

    add reading and unitreading

---
+++ Patient-Monitoring-System/reading.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patient_Monitoring_System
+{
+    public class reading
+    {
+        private string reading_id;
+        private double minBloodPressure;
+        private double maxBloodPressure;
+        private double minTemperature;
+        private double maxTemperature;
+        private double minPulse;
+        private double maxPulse;
+        private double minBreathing;
+        private double maxBreathing;
+
+        public string Reading_id { get => reading_id; set => reading_id = value; }
+        public double MinBloodPressure { get => minBloodPressure; set => minBloodPressure = value; }
+        public double MaxBloodPressure { get => maxBloodPressure; set => maxBloodPressure = value; }
+        public double MinTemperature { get => minTemperature; set => minTemperature = value; }
+        public double MaxTemperature { get => maxTemperature; set => maxTemperature = value; }
+        public double MinPulse { get => minPulse; set => minPulse = value; }
+        public double MaxPulse { get => maxPulse; set => maxPulse = value; }
+        public double MinBreathing { get => minBreathing; set => minBreathing = value; }
+        public double MaxBreathing { get => maxBreathing; set => maxBreathing = value; }
+    }
+}
---
+++ PatientMonitoringSystemUnitTest/UnitTestReadingHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Patient_Monitoring_System;
+
+namespace PatientMonitoringSystemUnitTest
+{
+    [TestClass]
+    public class UnitTestReadingHandler
+    {
+        [TestMethod]
+        public void AddReadingHandler()
+        {
+            reading readingclass = new reading();
+            Assert.IsInstanceOfType(readingclass, typeof(object));
+        }
+    }
+}
