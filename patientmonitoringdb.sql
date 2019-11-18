9efde0268457028bfcdec6a1ab326ccc9a4e5706
Raymond Loke <junioraymond92@gmail.com>
2019-11-18 12:44:55 +0800

    add database file - patientmonitoringdb.sql

---
+++ patientmonitoringdb.sql
@@ -0,0 +1,337 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 18, 2019 at 05:43 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `patientmonitoringdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alarm`
+--
+
+CREATE TABLE `alarm` (
+  `id` int(11) NOT NULL,
+  `reading_id` int(11) NOT NULL,
+  `alarmLevel` varchar(20) NOT NULL,
+  `dateTimeTrigger` datetime NOT NULL,
+  `dateTimeMuted` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bedsidemonitor`
+--
+
+CREATE TABLE `bedsidemonitor` (
+  `id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `centralStation_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bloodpressure`
+--
+
+CREATE TABLE `bloodpressure` (
+  `id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `bloodPressureValue` double NOT NULL,
+  `bloodPressureTime` time NOT NULL,
+  `bloodPressureDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `breathingrate`
+--
+
+CREATE TABLE `breathingrate` (
+  `id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `breathingRateValue` double NOT NULL,
+  `breathingRateTime` time NOT NULL,
+  `breathingRateDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `centralstation`
+--
+
+CREATE TABLE `centralstation` (
+  `id` int(11) NOT NULL,
+  `alarm_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `id` int(11) NOT NULL,
+  `reading_id` int(11) NOT NULL,
+  `fullName` varchar(20) NOT NULL,
+  `NRIC` int(30) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `phone` int(30) NOT NULL,
+  `age` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pulserate`
+--
+
+CREATE TABLE `pulserate` (
+  `id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `pulseRateValue` double NOT NULL,
+  `pulseRateTime` time NOT NULL,
+  `pulseRateDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reading`
+--
+
+CREATE TABLE `reading` (
+  `id` int(11) NOT NULL,
+  `minValueBloodPressure` double NOT NULL,
+  `maxValueBloodPressure` double NOT NULL,
+  `minValueTemperature` double NOT NULL,
+  `maxValueTemperature` double NOT NULL,
+  `minValueBreathingRate` double NOT NULL,
+  `maxValueBreathingRate` double NOT NULL,
+  `minValuePulseRate` double NOT NULL,
+  `maxValuePulseRate` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `user_account_id` int(11) NOT NULL,
+  `position` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temperature`
+--
+
+CREATE TABLE `temperature` (
+  `id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `temperatureValue` double NOT NULL,
+  `temperatureTime` time NOT NULL,
+  `temperatureDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_account`
+--
+
+CREATE TABLE `user_account` (
+  `id` int(11) NOT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `loginStatus` varchar(20) DEFAULT NULL,
+  `NRIC` int(30) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `phone` int(20) DEFAULT NULL,
+  `dateTimeRegister` datetime DEFAULT NULL,
+  `dateTimeLogin` datetime DEFAULT NULL,
+  `dateTimeLogOut` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alarm`
+--
+ALTER TABLE `alarm`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reading_id` (`reading_id`);
+
+--
+-- Indexes for table `bedsidemonitor`
+--
+ALTER TABLE `bedsidemonitor`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `patient_id` (`patient_id`),
+  ADD KEY `centralStation_id` (`centralStation_id`);
+
+--
+-- Indexes for table `bloodpressure`
+--
+ALTER TABLE `bloodpressure`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `patient_id` (`patient_id`);
+
+--
+-- Indexes for table `breathingrate`
+--
+ALTER TABLE `breathingrate`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `patient_id` (`patient_id`);
+
+--
+-- Indexes for table `centralstation`
+--
+ALTER TABLE `centralstation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `alarm_id` (`alarm_id`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reading_id` (`reading_id`);
+
+--
+-- Indexes for table `pulserate`
+--
+ALTER TABLE `pulserate`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `patient_id` (`patient_id`);
+
+--
+-- Indexes for table `reading`
+--
+ALTER TABLE `reading`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_account_id` (`user_account_id`);
+
+--
+-- Indexes for table `temperature`
+--
+ALTER TABLE `temperature`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `patient_id` (`patient_id`);
+
+--
+-- Indexes for table `user_account`
+--
+ALTER TABLE `user_account`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alarm`
+--
+ALTER TABLE `alarm`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `bedsidemonitor`
+--
+ALTER TABLE `bedsidemonitor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `bloodpressure`
+--
+ALTER TABLE `bloodpressure`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `breathingrate`
+--
+ALTER TABLE `breathingrate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `centralstation`
+--
+ALTER TABLE `centralstation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pulserate`
+--
+ALTER TABLE `pulserate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reading`
+--
+ALTER TABLE `reading`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `temperature`
+--
+ALTER TABLE `temperature`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_account`
+--
+ALTER TABLE `user_account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
