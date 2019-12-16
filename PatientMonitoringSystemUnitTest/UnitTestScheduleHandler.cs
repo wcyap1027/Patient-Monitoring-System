2537b595bd95cc2daa750ee919ca4d7e2b43fa8e
wcyap1027 <wcyap9810@gmail.com>
2019-12-16 11:13:21 +0800

    update unitTest

---
+++ PatientMonitoringSystemUnitTest/UnitTestScheduleHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Manager;
+
+namespace PatientMonitoringSystemUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddSchedule()
+        {
+            DbConnector dbCon = new DbConnector();
+            string result = dbCon.connect();
+            Assert.AreEqual("Done", result);
+            StaffSchedulerHandler staffhnd = new StaffSchedulerHandler();
+
+            Schedule schedule = new Schedule(); 
+        }
+    }
+}
